0d3fd7c53bca86105ad758ca171ab25ff6c46980
Šimon Lukašík <isimluk@fedoraproject.org>
2020-10-19 13:39:11 +0200

    Divide text in docx into paragraphs if needed
    This adds paragraphs to the controls with single response (single-part controls).

--- pkg/docx_helper/table.go
+++ pkg/docx_helper/table.go
@@ -4,7 +4,10 @@ import (
 	"errors"
 	"fmt"
 	"github.com/jbowtie/gokogiri/xml"
+	"strings"
 )
+
+const libxml2_copy_constant = 2
 
 func ParseTableHeaderContent(table xml.Node) (content string, err error) {
 	nodes, err := table.Search(".//w:tr")
@@ -29,6 +32,20 @@ func RowReplaceText(row xml.Node, newText string) error {
 	if len(paragraphNodes) != 1 {
 		return fmt.Errorf("Could not update a table row: found '%d' paragraph(s) in while expecting only 1.", len(paragraphNodes))
 	}
-	return ParagraphSetText(paragraphNodes[0], newText)
+	templateParagraph := paragraphNodes[0]
 
+	for _, text := range strings.Split(newText, "\n\n") {
+		clone := templateParagraph.Duplicate(libxml2_copy_constant)
+		err = ParagraphSetText(clone, text)
+		if err != nil {
+			return err
+		}
+		err = templateParagraph.Parent().AddChild(clone)
+		if err != nil {
+			return err
+		}
+	}
+
+	templateParagraph.Remove()
+	return nil
 }
