ba4952755799438cba7a9e63b0fbea462b789a16
Šimon Lukašík <isimluk@fedoraproject.org>
2020-10-19 14:58:54 +0200

    Refactor: Extract method: ParagraphReplaceWithText

--- pkg/docx_helper/paragraph.go
+++ pkg/docx_helper/paragraph.go
@@ -5,6 +5,8 @@ import (
 	"strings"
 )
 
+const libxml2_copy_constant = 2
+
 func ParagraphSetText(pNode xml.Node, text string) error {
 	existingR, err := pNode.Search(".//w:r")
 	if err != nil {
@@ -21,4 +23,20 @@ func ParagraphSetText(pNode xml.Node, text string) error {
 
 	text = strings.TrimSpace(text)
 	return textCell.SetContent(text)
+}
+
+func ParagraphReplaceWithText(originalParagraph xml.Node, newText string) error {
+	for _, text := range strings.Split(newText, "\n\n") {
+		clone := originalParagraph.Duplicate(libxml2_copy_constant)
+		if err := ParagraphSetText(clone, text); err != nil {
+			return err
+		}
+		if err := originalParagraph.Parent().AddChild(clone); err != nil {
+			return err
+		}
+	}
+
+	originalParagraph.Remove()
+	return nil
+
 }
--- pkg/docx_helper/table.go
+++ pkg/docx_helper/table.go
@@ -4,10 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/jbowtie/gokogiri/xml"
-	"strings"
 )
-
-const libxml2_copy_constant = 2
 
 func ParseTableHeaderContent(table xml.Node) (content string, err error) {
 	nodes, err := table.Search(".//w:tr")
@@ -32,20 +29,5 @@ func RowReplaceText(row xml.Node, newText string) error {
 	if len(paragraphNodes) != 1 {
 		return fmt.Errorf("Could not update a table row: found '%d' paragraph(s) in while expecting only 1.", len(paragraphNodes))
 	}
-	templateParagraph := paragraphNodes[0]
-
-	for _, text := range strings.Split(newText, "\n\n") {
-		clone := templateParagraph.Duplicate(libxml2_copy_constant)
-		err = ParagraphSetText(clone, text)
-		if err != nil {
-			return err
-		}
-		err = templateParagraph.Parent().AddChild(clone)
-		if err != nil {
-			return err
-		}
-	}
-
-	templateParagraph.Remove()
-	return nil
+	return ParagraphReplaceWithText(paragraphNodes[0], newText)
 }
