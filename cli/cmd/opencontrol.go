3d5b222d0b4bdefac4dc1cf0460868f79a10da1a
Šimon Lukašík <isimluk@fedoraproject.org>
2020-10-27 14:06:16 +0100

    wip: introduce --format flag to be able to export json or yaml along the xml

--- cli/cmd/opencontrol.go
+++ cli/cmd/opencontrol.go
@@ -5,12 +5,22 @@ import (
 	"github.com/urfave/cli"
 )
 
+var format string
+
 // ConvertOpenControl ...
 var openControl = cli.Command{
 	Name:        "opencontrol",
 	Usage:       `Convert OpenControl masonry repo into FedRAMP formatted OSCAL`,
 	Description: `Convert OpenControl masonry repository into FedRAMP formatted OSCAL SSP Documents`,
 	ArgsUsage:   "[masonry-repository] [output-directory]",
+	Flags: []cli.Flag{
+		cli.StringFlag{
+			Name:        "format, f",
+			Usage:       "Format of the output: xml, json, or yaml",
+			Value:       "xml",
+			Destination: &format,
+		},
+	},
 	Before: func(c *cli.Context) error {
 		if c.NArg() != 2 {
 			return cli.NewExitError("Missing masonry repository or output directory", 1)
