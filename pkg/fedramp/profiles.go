148ae7c033b1b92c9b37747d2e52b716cf61d786
Šimon Lukašík <isimluk@fedoraproject.org>
2020-10-29 13:53:26 +0100

    Profile is no longer needed for the baseline processing
    Let's remove it for now.

--- pkg/fedramp/profiles.go
+++ pkg/fedramp/profiles.go
@@ -7,42 +7,17 @@ import (
 	"github.com/gocomply/oscalkit/pkg/oscal/constants"
 	"github.com/gocomply/oscalkit/pkg/oscal_source"
 	"github.com/gocomply/oscalkit/types/oscal/catalog"
-	"github.com/gocomply/oscalkit/types/oscal/profile"
 )
 
 type Baseline struct {
 	Level   common.BaselineLevel
-	profile *profile.Profile
 	catalog *catalog.Catalog
 }
 
 func NewBaseline(baselineLevel common.BaselineLevel) (*Baseline, error) {
 	var result Baseline
 	result.Level = baselineLevel
-	err := result.loadProfile()
-	if err != nil {
-		return nil, err
-	}
 	return &result, result.loadCatalog()
-}
-
-func (baseline *Baseline) loadProfile() error {
-	file, err := bundled.ProfileOSCAL(baseline.Level)
-	if err != nil {
-		return fmt.Errorf("could not initiate FedRAMP: could not open internal files: %v", err)
-	}
-	defer file.Close()
-	source, err := oscal_source.OpenFromReader(file.Name(), file)
-	if err != nil {
-		return err
-	}
-	defer source.Close()
-	oscal := source.OSCAL()
-	if oscal.DocumentType() != constants.ProfileDocument {
-		return fmt.Errorf("Could not initiate FedRAMP. Expected profile element in %s", file.Name())
-	}
-	baseline.profile = oscal.Profile
-	return nil
 }
 
 func (baseline *Baseline) loadCatalog() error {
