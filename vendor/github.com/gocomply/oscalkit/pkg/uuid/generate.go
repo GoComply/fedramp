4dd537c5a1ef138aef40a78381a0325bf44720b2
Šimon Lukašík <isimluk@fedoraproject.org>
2020-11-22 16:00:22 +0100

    Update oscalkit dependency to the latest greatest

---
+++ vendor/github.com/gocomply/oscalkit/pkg/uuid/generate.go
@@ -0,0 +1,36 @@
+package uuid
+
+import (
+	"bytes"
+	"crypto/sha1"
+	"encoding/json"
+	"github.com/google/uuid"
+)
+
+type Uuindexable interface {
+	SetUuid(uuid string)
+}
+
+// Generate function returns UUID (rfc 4122) compliant string suitable
+// for use within OSCAL documents. OSCAL requires random based (version 4)
+// UUID to be used. However it is clear that having stable UUIDs (that is
+// same id for same content) is highly beneficial. This method creates
+// version 4 UUIDs that are not truly random but rather content dependent.
+func Refresh(element Uuindexable) error {
+	element.SetUuid("")
+	signature, err := marshal(element)
+	if err != nil {
+		return err
+	}
+	id := uuid.NewHash(sha1.New(), uuid.Nil, signature, 4)
+	element.SetUuid(id.String())
+	return nil
+}
+
+func marshal(data interface{}) ([]byte, error) {
+	buffer := bytes.Buffer{}
+	encoder := json.NewEncoder(&buffer)
+	err := encoder.Encode(data)
+	return buffer.Bytes(), err
+
+}
---
+++ vendor/github.com/gocomply/oscalkit/types/oscal/system_security_plan/uuindexable.go
@@ -0,0 +1,23 @@
+package system_security_plan
+
+func (bc *ByComponent) SetUuid(uuid string) {
+	bc.Uuid = uuid
+}
+func (c *Component) SetUuid(uuid string) {
+	c.Uuid = uuid
+}
+
+func (ir *ImplementedRequirement) SetUuid(uuid string) {
+	ir.Uuid = uuid
+}
+
+func (st *Statement) SetUuid(uuid string) {
+	st.Uuid = uuid
+}
+func (ssp *SystemSecurityPlan) SetUuid(uuid string) {
+	ssp.Uuid = uuid
+}
+
+func (u *User) SetUuid(uuid string) {
+	u.Uuid = uuid
+}
